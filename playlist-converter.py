c84a798bc9f823eb80cd453c4fb764ffacded20d
Nathanial Howard <nathanial55@gmail.com>
2022-12-23 21:57:00 +0000

    Feature improvements

--- playlist-converter.py
+++ playlist-converter.py
@@ -7,6 +7,7 @@ import m3u8
 import argparse
 import glob
 import os
+import signal
 import re
 
 _to_esc = re.compile(r'\s|[]()[]')
@@ -17,26 +18,33 @@ def my_escape(name):
 
 def load_playlist(args):
     playlist = m3u8.load(args.playlist)
-    print(playlist.segments)
 
     files = playlist.files
+    count = 0
     for file in files:
         # Fix paths to /mnt/f for running in wsl
         file = my_escape(subprocess.run(["wslpath", "-a", file], stdout=PIPE, stderr=PIPE, universal_newlines=True).stdout.strip())
         new_file=file.replace('flac', 'mp3')
 
         # Using os.system because subprocess added weird \\ double backslashes to path variables
-        print(f"Converting {file} to {new_file}")
-        command = "ffmpeg -i "+file+" -ab 320k -codec:a libmp3lame -q:a 0 -map_metadata 0 -id3v2_version 3 -write_id3v1 1 "+new_file
-        print(command)
-        os.system(command)
+        print(f"{count}/{len(files)} Converting {file} to {new_file}")
+        command = "ffmpeg -i "+file+" -hide_banner -loglevel panic -ab 320k -codec:a libmp3lame -q:a 0 -map_metadata 0 -id3v2_version 3 -write_id3v1 1 "+new_file
+        if args.overwrite == True:
+            command+=" -y"
+
+        run = os.system(command)
+        if run == signal.SIGINT:
+            break
+
+        count = count + 1
 
 if __name__ == "__main__":
     # Entrypoint
     parser = argparse.ArgumentParser(
-                        prog = 'Playlist Converter',
-                        description = 'Converts a playlist (m3u8) file contents in place using ffmpeg')
+        prog = 'Playlist Converter',
+        description = 'Converts a playlist (m3u8) file contents in place using ffmpeg')
     parser.add_argument('playlist')
+    parser.add_argument('-y', '--overwrite', action='store_true')
     args = parser.parse_args()
 
     # Validate args
